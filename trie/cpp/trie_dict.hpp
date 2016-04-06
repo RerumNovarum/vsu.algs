7cca84db8386b33faaa65f620c1a001632d78cd7
Sergey V Kozlukov <rerumnovarum@openmailbox.org>
2016-04-07 00:23:14 +0300

    trie moved to cmake

---
+++ trie/cpp/trie_dict.hpp
@@ -0,0 +1,50 @@
+#include <stdexcept>
+#include <map>
+
+template<typename V>
+class Trie {
+  private:
+    std::map<char, Trie<V>*> next;
+    bool word;
+    V v;
+
+    Trie<V>* find(std::string &key) {
+      auto t = this;
+      int i = 0;
+      int end = key.size();
+      while (t != nullptr && i != end) {
+        auto it = t->next.find(key[i]);
+        if (it == t->next.end()) break;
+        t = *it;
+        i += 1;
+      }
+      if (i == end) return t;
+      return nullptr;
+    }
+
+  public:
+    bool has_key(std::string &key) {
+      auto t = this->find(key);
+      return t != nullptr && t->word;
+    }
+
+    V& at(std::string &key) {
+      auto t = this->find(key);
+      if (t == nullptr || !t->word) throw std::out_of_range("No such key");
+      return t->v;
+    }
+
+    V& operator[] (std::string key) {
+      auto t = this;
+      int i = 0;
+      int end = key.size();
+      while(i != end) {
+        char c = key[i];
+        if (t->next.find(c) == t->next.end()) t->next[c] = new Trie<V>();
+        t = t->next[c];
+        i += 1;
+      }
+      t->word = true;
+      return t->v;
+    }
+};
---
+++ trie/cpp/trie_dict_client.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "trie_dict.hpp"
+
+using namespace std;
+
+int main() {
+  return 0;
+}
